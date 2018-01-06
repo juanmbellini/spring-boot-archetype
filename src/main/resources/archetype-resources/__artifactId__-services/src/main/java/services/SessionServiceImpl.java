b0537470f3f5dd7da2dc5fb2adb5ea9832f12f99
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-01-06 03:24:21 -0300

    Fix typo in SessionServiceImpl javadoc (closes #17)

--- src/main/resources/archetype-resources/__artifactId__-services/src/main/java/services/SessionServiceImpl.java
+++ src/main/resources/archetype-resources/__artifactId__-services/src/main/java/services/SessionServiceImpl.java
@@ -37,7 +37,7 @@ public class SessionServiceImpl implements SessionService {
     private final SessionQueryHelper sessionQueryHelper;
 
     /**
-     * {@link CurrentUserIdProvider} to know the currenly authenticated user id.
+     * {@link CurrentUserIdProvider} to know the currently authenticated user id.
      */
     private final CurrentUserIdProvider currentUserIdProvider;
 
